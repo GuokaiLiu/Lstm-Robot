386bae8e64d6754b5c55e9ed7a808d5b48b14fbe
brucelau <guokai_liu@163.com>
2019-04-02 15:03:17 +0800

    Finihs Example with [1,20] data

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
-
-import pandas as pd
-import numpy as np
+from math import sqrt
+from numpy import concatenate
+from matplotlib import pyplot
+from pandas import read_csv
 from pandas import DataFrame
 from pandas import concat
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import numpy as np
+
 
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	n_vars = 1 if type(data) is list else data.shape[1]
@@ -28,13 +37,55 @@ def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
 	return agg
 
 
+data = np.linspace(1,20,num=20).reshape((10,2))
+scaler = MinMaxScaler(feature_range=(0,1))
+
+scaled = scaler.fit_transform(data)
+
+n_steps_in = 3
+n_steps_ou = 2
+n_features = 2
 
 
 
-df = pd.DataFrame({'Col1': [10, 20, 15, 30, 45],
-'Col2': [13, 23, 18, 33, 48],
-'Col3': [17, 27, 22, 37, 52]})
+
+reformed = series_to_supervised(data,n_steps_in,n_steps_ou) 	# without MinMaxScaler
+reformed = series_to_supervised(scaled,n_steps_in,n_steps_ou) 	# with MinMaxScaler
+print(reformed.shape) # m: L - (n_in + n_out) + 1, n = (n_in + n_out)*n_features
+reformed = reformed.values
+
+train = reformed[::2]  # Get all the odd lines
+test = reformed[1::2]  # Get all the even lines
 
 
-reformed = series_to_supervised(df.values,3)
+train_X = train[:, :6]
+train_y = train[:,6:]
+test_X = test[:,:6]
+test_y = test[:,6:]
 
+
+train_X = train_X.reshape((train_X.shape[0], n_steps_in, n_features))
+test_X = test_X.reshape((test_X.shape[0], n_steps_in, n_features))
+
+
+# train_y = train_y.reshape((train_y.shape[0], n_steps_ou, n_features))
+# test_y = test_y.reshape((test_y.shape[0], n_steps_ou, n_features))
+train_y = train_y[:,0]
+test_y = test_y[:,0]
+print('The training data shape is ', train_X.shape)
+print('The testing data shape is ', test_X.shape)
+
+
+# Evaluate Model
+# design network
+model = Sequential()
+model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mae', optimizer='adam')
+# fit network
+history = model.fit(train_X, train_y, epochs=50, batch_size=2, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+# plot history
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
